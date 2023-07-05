09f5e8368c909bb25ee1d7dc531b56ffa634be34
Bedram Tamang <bedram@jobins.jp>
2023-07-06 01:07:38 +0545

    feat: next and previous feature

--- resources/views/pages/admin/Translation/Edit.vue
+++ resources/views/pages/admin/Translation/Edit.vue
@@ -1,76 +1,95 @@
 <template>
     <div class="container mx-auto mt-20">
         <form @submit.prevent="submit">
-            <div class="mb-4">
-                <label>In English</label>
-                <textarea v-model="form.english"
-                          class="z-100 w-full text-2xl h-36 border focus:ring-0 focus:outline-none"/>
-                <InputError :message="form.errors.english"/>
+            <div class="grid grid-cols-3 gap-2">
+                <div class="mb-4">
+                    <label>In English</label>
+                    <textarea v-model="form.english"
+                              class="z-100 w-full text-2xl h-36 border rounded focus:ring-0 focus:outline-none"/>
+                    <InputError :message="form.errors.english"/>
+                </div>
+
+                <div class="mb-4">
+                    <label>In Limbu</label>
+                    <InputLimbu v-model="form.limbu"/>
+                    <InputError :message="form.errors.limbu"/>
+                </div>
+
+                <div class="mb-4">
+                    <label>Pronunciation</label>
+                    <textarea v-model="form.pronunciation"
+                              class="z-100 w-full text-2xl h-36 border rounded focus:ring-0 focus:outline-none"/>
+                    <InputError :message="form.errors.pronunciation"/>
+                </div>
             </div>
 
-            <div class="mb-4">
-                <label>In Limbu</label>
-                <InputLimbu v-model="form.limbu"/>
-                <InputError :message="form.errors.limbu"/>
-            </div>
-
-            <div class="mb-4">
-                <label>Pronunciation</label>
-                <textarea v-model="form.pronunciation"
-                          class="z-100 w-full text-2xl h-36 border focus:ring-0 focus:outline-none"/>
-                <InputError :message="form.errors.pronunciation"/>
-            </div>
-
-            <div class="flex justify-end">
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Submit</button>
+            <div class="flex justify-between">
+                <div class="grid grid-cols-2 gap-2">
+                    <Link :href="`/translations/${translation.id-1}`"
+                          class="text-center border border-blue-500 text-blue-500 px-4 py-2 rounded">
+                        Previous
+                    </Link>
+                    <Link :href="`/translations/${translation.id+1}`"
+                          class="text-center border border-blue-500 text-blue-500 px-4 py-2 rounded">
+                        Next
+                    </Link>
+                </div>
+                <button type="submit" class="ml-2 bg-blue-500 text-white px-4 py-2 rounded">
+                    Update
+                </button>
             </div>
         </form>
     </div>
 </template>
 
 <script lang="ts">
-    import InputError from "@/views/components/forms/InputError.vue"
-    import InputLimbu from "@/views/components/forms/InputLimbu.vue"
-    import { useForm } from "@inertiajs/inertia-vue3"
-    import { defineComponent, PropType, watch, watchEffect } from "vue"
-    import Layout from "../../../layouts/default.vue"
+import InputError from "@/views/components/forms/InputError.vue"
+import InputLimbu from "@/views/components/forms/InputLimbu.vue"
+import {InertiaForm, Link, useForm} from "@inertiajs/inertia-vue3"
+import {defineComponent, PropType, watch} from "vue"
+import Layout from "../../../layouts/default.vue"
 
-    export default defineComponent({
-        components: { InputLimbu, InputError },
-        layout: Layout,
+export default defineComponent({
+    components: {InputLimbu, InputError, Link},
+    layout: Layout,
 
-        props: {
-            translation: {
-                type: Object as PropType<{
-                    id: Number,
-                    english: String, limbu: String,
-                    pronunciation: String
-                }>,
-                required: true,
-            },
+    props: {
+        translation: {
+            type: Object as PropType<{
+                id: Number,
+                english: String,
+                limbu: String,
+                pronunciation: String
+            }>,
+            required: true,
         },
+    },
 
-        setup(props) {
-            const form = useForm({
-                english: "",
-                limbu: "",
-                pronunciation: "",
-            })
-            
-            watch(() => props.translation, () => {
-                form.english = props.translation.english
-                form.limbu = props.translation.limbu
-                form.pronunciation = props.translation.pronunciation
-            }, { immediate: true, deep: true })
+    setup(props) {
+        const form: InertiaForm<{
+            english: String,
+            limbu: String,
+            pronunciation: String
+        }> = useForm({
+            english: "",
+            limbu: "",
+            pronunciation: "",
+        })
 
-            const submit = () => {
-                form.put(`/translations/${props.translation.id}`)
-            }
+        watch(() => props.translation, () => {
+            form.english = props.translation.english
+            form.limbu = props.translation.limbu
+            form.pronunciation = props.translation.pronunciation
+        }, {immediate: true, deep: true})
 
-            return {
-                form,
-                submit,
-            }
-        },
-    })
+        const submit = () => {
+            form.put(`/translations/${props.translation.id}`)
+        }
+
+        return {
+            form,
+            submit,
+        }
+    },
+})
 </script>
---
+++ tests/Unit/Console/Commands/ImportCsvCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit\Console\Commands;
+
+use App\Models\Translation;
+use Spatie\SimpleExcel\SimpleExcelReader;
+use Tests\TestCase;
+
+class ImportCsvCommandTest extends TestCase
+{
+    public function testItImportFiles()
+    {
+        $this->assertTrue(true);
+    }
+}
